4acb05f9e6a6a291aaffd42a004262ef5378db80
Jens Rud Nielsen <jenrud@gmail.com>
2021-02-02 22:18:35 +0100

    Adds console

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HtmlAgilityPack;
 
 namespace webscraber
 {
@@ -6,7 +9,41 @@ namespace webscraber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Web scraber");
+            Console.WriteLine("Gets links with target blank and without rel=\"noopener noreferrer\"");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            var baseUrl = "https://dk.sunclassairlines.dk";
+
+            List<string> urls = new List<string>();
+            urls.Add(baseUrl);
+
+            // get pages
+            var web = new HtmlWeb();
+            HtmlDocument frontpage = web.Load(baseUrl);
+            var div = frontpage.DocumentNode.SelectSingleNode("//nav[@class=\"navbar navbar-default\"]");
+            if (div != null)
+            {
+                urls.AddRange(div.Descendants("a").Select(node => node.GetAttributeValue("href", string.Empty)).ToList());
+            }
+
+            // Normalize urls
+            urls = urls.Select(x => x.StartsWith("/") ? baseUrl + x.TrimEnd('/') : x.TrimEnd('/')).Distinct().ToList();
+
+            foreach (var url in urls)
+            {
+                HtmlDocument doc = web.Load(url);
+                foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+                {
+                    // do something with link here
+                    if (link.Attributes["target"]?.Value == "_blank" && link.Attributes["rel"] == null)
+                    {
+                        Console.WriteLine(url + " : " + link.OuterHtml);
+                    }
+                }
+            }
+
+            Console.ResetColor();
         }
     }
 }
