8f8f5eb8650ebf9c50212ba01e1b797246cfec35
Jens Rud Nielsen <jenrud@gmail.com>
2021-02-02 21:27:29 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace webscraber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
